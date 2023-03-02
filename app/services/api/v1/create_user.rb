89ba0d1ce5aebce5d9d8a50c3e43e995f3d2aed2
quasi-presence <quasi_presence@yahoo.com>
2023-03-01 20:18:46 -0800

    Create Api::V1::CreateUser service

---
+++ app/services/api/v1/create_user.rb
@@ -0,0 +1,23 @@
+class Api::V1::CreateUser
+  class << self
+    def call(params)
+      user = User.new(params)
+      user.save ? service_data(user) : error_data(user)
+    end
+
+    private
+
+    def service_data(user)
+      {
+        'id' => user.id,
+        'email' => user.email,
+        'created_at' => user.created_at,
+        'updated_at' => user.updated_at
+      }
+    end
+
+    def error_data(user)
+      { 'errors' => user.errors.full_messages }
+    end
+  end
+end
---
+++ spec/services/api/v1/create_user_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::CreateUser, type: :service do
+  describe '.call' do
+    let(:result) { Api::V1::CreateUser.call(params) }
+
+    context 'with valid params' do
+      let(:params) { { 'email' => 'joe.user@test.com', 'password_digest' => 'fakepassword' } }
+
+      it 'creates user and returns data' do
+        expect { result }.to change { User.count }.by(1)
+
+        user = User.find_by(email: params['email'])
+        expect(result).to eq({
+          'id' => user.id,
+          'email' => user.email,
+          'created_at' => user.created_at,
+          'updated_at' => user.updated_at
+        })
+      end
+    end
+
+    context 'with invalid params' do
+      let(:params) { { 'email' => 'joe.user@test.com', 'password_digest' => nil } }
+
+      it 'fails to create user and returns errors' do
+        expect { result }.to change { User.count }.by(0)
+
+        user = User.find_by(email: params['email'])
+        expect(result).to eq({ 'errors' => ["Password can't be blank"] })
+      end
+    end
+  end
+end
